450c9f368709dd9fb930bfea046396a22fb2471d
Takashi Futada <tafutada@gmail.com>
2015-12-26 12:45:51 +0700

    Init

---
+++ src/S08HighOrder.scala
@@ -0,0 +1,28 @@
+
+object S08HighOrder {
+
+    def main(args: Array[String]) = {
+
+        val list = List(1, 2, 3, 4, 5)
+
+        // map: x => x * 2
+        println( list.map( x => x * 2) )
+
+        // filter: x => x * 2
+        println( list.filter( x => x % 2 == 0) )
+
+        // reduce: x, y => x + y
+        println( list.reduce( (x, y) => x + y ) )
+
+        // map.reduce
+        println( list
+            .map(x => x * 2)
+            .reduce( (x, y) => x + y )
+        )
+    }
+}
+
+
+
+
+
---
+++ src/S09Class.scala
@@ -0,0 +1,15 @@
+
+class Person(first: String, last: String, age: Int)
+
+object S09Class {
+
+    def main(args: Array[String]) = {
+
+        val david = new Person("David", "Thompson", 29)
+    }
+}
+
+
+
+
+
